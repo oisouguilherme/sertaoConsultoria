fe6c9fb9181e7eac0fbf6c6a540e3f48e97c036c
Guilherme Henrique <gh.dourado77@gmail.com>
2023-04-18 14:15:47 -0300

    ajustando video no celular

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -5,7 +5,7 @@ export function Main() {
   return (
     <section id="Main" className=" bg-image w-full md:p-44 p-12 flex flex-col items-center gap-24 relative ">
       <video 
-      className="absolute top-0 h-full w-full object-cover"
+      className="absolute top-0 h-full w-full object-cover hidden md:flex"
       src={video} 
       autoPlay
       muted
