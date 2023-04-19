87f3da3934c607f60019fad26891f378ac534110
Guilherme Henrique <gh.dourado77@gmail.com>
2023-04-19 15:34:59 -0300

    Update index.jsx

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,7 +2,10 @@ import "./Main.css";
 export function Main() {
   return (
     <section id="Main" className=" bg-image w-full md:py-52 p-12 flex flex-col items-center gap-24 ">
-      <h1 className="text-white font-black text-6xl text-center z-10">
+      <h1 
+      data-aos="fade-up"
+      data-aos-delay="600"
+      className="text-white font-black text-6xl text-center z-10">
         Deixe sua empresa dentro da legislação ambiental e cresça sem riscos!
       </h1>
     </section>
