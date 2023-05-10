ba55222318d133ba62c59035245090aaf668668b
Guilherme Henrique <gh.dourado77@gmail.com>
2023-05-10 19:01:52 -0300

    alterando txt

--- src/components/NossaHistoria/index.jsx
+++ src/components/NossaHistoria/index.jsx
@@ -17,7 +17,7 @@ export function NossaHistoria() {
           data-aos-delay="300"
           className="text-2xl"
         >
-          Ianna e João Rafael decidiram juntos usar o conhecimento que acumularam ao longo dos anos para atender diversas empresas e fundar a Sertão consultoria. Juntos, somam mais de 14 anos de trabalhos realizados na área ambiental. No início o projeto era ambicioso; atender empresas de diversos seguimentos e tamanhos para que pudessem estar em conformidade com a legislação ambiental. Não acreditamos apenas em cumprimento de normas, e sim o atendimento delas como parte importante no processo para que as empresas atendidas se tornassem mais sólidas, fortes e referência no mercado em que atuam. Hoje, várias empresas foram atendidas pela Sertões e obedecem a legislação ambiental para crescerem sem risco.
+          Ianna e João Rafael decidiram juntos usar o conhecimento que acumularam ao longo dos anos para atender diversas empresas e fundar a Sertões consultoria. Juntos, somam mais de 14 anos de trabalhos realizados na área ambiental. No início o projeto era ambicioso; atender empresas de diversos seguimentos e tamanhos para que pudessem estar em conformidade com a legislação ambiental. Não acreditamos apenas em cumprimento de normas, e sim o atendimento delas como parte importante no processo para que as empresas atendidas se tornassem mais sólidas, fortes e referência no mercado em que atuam. Hoje, várias empresas foram atendidas pela Sertões e obedecem a legislação ambiental para crescerem sem risco.
         </p>
       </div>
       <img
